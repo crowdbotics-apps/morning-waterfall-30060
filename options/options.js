45d08f37cc5bd66fb3b494a3162bb2273523f91c
Crowdbotics <team@crowdbotics.com>
2021-08-23 23:51:21 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "morning_waterfall_30060Identifier",
+  url: "https://morning_waterfall_30060Identifier.botics.co",
+  api: "https://morning_waterfall_30060Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
